5a7b82f8515efbe4b15283069f71620b06e0d613
Declan Warn <declan.warn@gmail.com>
2019-11-01 17:10:28 +1100

    Implement portals moving player

--- src/unsw/dungeon/Portal.java
+++ src/unsw/dungeon/Portal.java
@@ -1,13 +1,57 @@
 package unsw.dungeon;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javafx.scene.image.Image;
 
-public class Portal extends Entity {
+public class Portal extends Entity implements EventHandler<MovementEvent>, EventEmitter<PlayerReachedPortalEvent> {
 	
 	public static Image img = new Image("/portal.png");
+	
+	private int linkId;
+	private Set<EventHandler<PlayerReachedPortalEvent>> reachedListeners;
 
-	public Portal(int x, int y) {
+	public Portal(int x, int y, int linkId) {
 		super(x, y);
+		this.linkId = linkId;
+		this.reachedListeners = new HashSet<>();
+	}
+	
+	public int getLinkId() {
+		return this.linkId;
+	}
+	
+	public boolean hasLinkId(int linkId) {
+		return this.linkId == linkId;
+	}
+	
+	@Override
+	public void onDungeonLoad(Dungeon dungeon) {
+		super.onDungeonLoad(dungeon);
+		dungeon.registerPortal(this);
+		dungeon.getPlayer().onMovement(this);
+	}
+
+	@Override
+	public void handle(MovementEvent event) {
+		if (event.getX() == getX() && event.getY() == getY()) {
+			this.broadcast(new PlayerReachedPortalEvent(this, event));
+		}
+	}
+
+	@Override
+	public void addListener(EventHandler<PlayerReachedPortalEvent> eventHandler) {
+		this.reachedListeners.add(eventHandler);
+	}
+
+	@Override
+	public void removeListener(EventHandler<PlayerReachedPortalEvent> eventHandler) {
+		this.reachedListeners.remove(eventHandler);
+	}
+	
+	private void broadcast(PlayerReachedPortalEvent event) {
+		this.reachedListeners.forEach(listener -> listener.handle(event));
 	}
 
 }
---
+++ src/unsw/dungeon/PortalNetwork.java
@@ -0,0 +1,38 @@
+package unsw.dungeon;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
+public class PortalNetwork {
+
+	private Map<Integer, List<Portal>> portals;
+	
+	public PortalNetwork() {
+		this.portals = new HashMap<>();
+	}
+	
+	public void register(Portal portal) {
+		this.portals.putIfAbsent(portal.getLinkId(), new ArrayList<>());
+		this.portals.get(portal.getLinkId()).add(portal);
+		
+		portal.addListener(new EventHandler<PlayerReachedPortalEvent>() {
+			@Override
+			public void handle(PlayerReachedPortalEvent event) {
+				Portal link = getLink(event.getPortal());
+				event.getMovement().cancel();
+				event.getEntity().setPos(link.getX(), link.getY());
+			}
+		});
+	}
+	
+	private Portal getLink(Portal portal) {
+		List<Portal> linked = this.portals.get(portal.getLinkId());
+		int pos = linked.indexOf(portal);
+		int size = linked.size();
+		
+		return linked.get((pos + 1) % size);
+	}
+	
+}
