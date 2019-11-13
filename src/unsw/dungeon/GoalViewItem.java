5ad1e3e364ac0720fe5934755fdb014bff2d075f
Declan Warn <declan.warn@gmail.com>
2019-11-14 01:33:13 +1100

    Add checkboxes to goal items

--- src/unsw/dungeon/GoalViewItem.java
+++ src/unsw/dungeon/GoalViewItem.java
@@ -1,7 +1,10 @@
 package unsw.dungeon;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.FontSmoothingType;
 import javafx.scene.text.Text;
@@ -11,12 +14,24 @@ public class GoalViewItem extends VBox {
 	
 	public static final int INDENT = 16;
 	
+	private HBox box;
+	private CheckBox check;
 	private Text textNode;
 	
 	public GoalViewItem() {
+		this.box = new HBox();
+		this.box.setAlignment(Pos.CENTER_LEFT);
+		this.box.setSpacing(4);
+		
+		this.check = new CheckBox();
+		this.check.setIndeterminate(false);
+		this.check.setDisable(true);
+		
 		this.textNode = new Text();
-		this.getChildren().add(textNode);
 		this.textNode.setFont(FontManager.getBodyFont(16));
+		
+		this.box.getChildren().addAll(check, textNode);
+		this.getChildren().add(box);
 	}
 	
 	public GoalViewItem(Goal goal, int level) {
