69e19dd5171f4a337e9056299898488a03e5bf8a
Declan Warn <declan.warn@gmail.com>
2019-11-16 19:55:18 +1100

    Use canvas for vignette

--- src/unsw/dungeon/Vignette.java
+++ src/unsw/dungeon/Vignette.java
@@ -3,32 +3,43 @@ package unsw.dungeon;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.scene.Group;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
-import unsw.dungeon.entity.Displayable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.RadialGradient;
 import unsw.dungeon.entity.Entity;
 import unsw.dungeon.entity.Player;
 
 public class Vignette extends Group {
 	
-	private final int TILE_WIDTH = 32;
-	private final int SEE_RADIUS = 5;
+	private final int TILE_SIZE = 32;
+	private final int SEE_ACROSS = 11;
+	
+	// The amount of overlap between the visible area and the rest of the
+	// overlay.
+	// Lower amounts can cause visual artifacts from floating point errors (I assume).
+	// Higher amounts make the circle look less circular.
+	private final int FUZZ = 4;
 
 	private int width;
 	private int height;
+	private int x;
+	private int y;
 	
-	public Vignette(Player ply, int width, int height) {		
-		this.width = width;
-		this.height = height;
+	private GraphicsContext gc;
+	
+	public Vignette(Player ply, int tilesWide, int tilesHigh) {
+		this.width = tilesWide * TILE_SIZE;
+		this.height = tilesHigh * TILE_SIZE;
 		
-		this.track(ply);
+		Canvas canvas = new Canvas(width, height);
+		this.gc = canvas.getGraphicsContext2D();		
 		
-		ImageView img = new ImageView(new Image("/overlay.png"));
-		this.getChildren().add(img);
+		this.getChildren().add(canvas);
 		
 		this.setX(ply.getX());
 		this.setY(ply.getY());
+		this.track(ply);
 	}
 	
 	private void track(Entity ent) {
@@ -50,11 +61,24 @@ public class Vignette extends Group {
 	}
 	
 	private void setX(int val) {
-		this.setTranslateX((val - SEE_RADIUS) * TILE_WIDTH);
+		this.x = val;
+		this.draw();
 	}
 	
 	private void setY(int val) {
-		this.setTranslateY((val - SEE_RADIUS) * TILE_WIDTH);
+		this.y = val;
+		this.draw();
+	}
+	
+	private void draw() {		
+		gc.clearRect(0, 0, width, height);
+		
+		gc.setFill(Color.BLACK);
+		gc.fillRect(0, 0, width, height);
+		gc.clearRect((x - SEE_ACROSS/2) * TILE_SIZE, (y - SEE_ACROSS/2) * TILE_SIZE, SEE_ACROSS * TILE_SIZE, SEE_ACROSS * TILE_SIZE);
+		
+		gc.setFill(RadialGradient.valueOf("radial-gradient(center 50% 50%, radius 50%, transparent 75%, black)"));
+		gc.fillRect((x - SEE_ACROSS/2) * TILE_SIZE - FUZZ, (y - SEE_ACROSS/2) * TILE_SIZE - FUZZ, SEE_ACROSS * TILE_SIZE + FUZZ * 2, SEE_ACROSS * TILE_SIZE + FUZZ * 2);
 	}
 	
 }
